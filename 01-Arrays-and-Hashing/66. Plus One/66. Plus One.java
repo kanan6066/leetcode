14575ae3d1171beeec5a056ef9adc3eeecfdcc52
Kanan Sharma <kanan2005sharma@gmail.com>
2026-08-18 11:49:57 +0530

    [66] [Time Beats: 100.00%] [Memory Beats: 62.18%] - Leet2Hub

--- 01-Arrays-and-Hashing/66. Plus One/66. Plus One.java
+++ 01-Arrays-and-Hashing/66. Plus One/66. Plus One.java
@@ -1,1 +1,16 @@
-class Solution {    public int[] plusOne(int[] digits) {        for (int i = digits.length - 1; i >= 0; i--) {            if (digits[i] < 9) {                digits[i]++;                return digits;            }            digits[i] = 0;        }        int[] result = new int[digits.length + 1];        result[0] = 1;        return result;    }}
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3
+4        for (int i = digits.length - 1; i >= 0; i--) {
+5            if (digits[i] < 9) {
+6                digits[i]++;
+7                return digits;
+8            }
+9            digits[i] = 0;
+10        }
+11        int[] result = new int[digits.length + 1];
+12        result[0] = 1;
+13        return result;
+14    }
+15}
+16
